31f5648e59f570dbecdaa182e2309de6d8e73f89
Stephen Sabo <sabo.stephen@gmail.com>
2019-09-05 11:29:10 +0100

    Add define aliased adapters for streamamp and totaljobs

--- headerBidder.js
+++ headerBidder.js
@@ -21,7 +21,7 @@ node.parentNode.insertBefore(gptLib, node);
 var prebid = document.createElement('script');
 prebid.type = 'text/javascript';
 prebid.async = true;
-prebid.src = '//static.amp.services/prebid' + (streamampConfig.prebidJsVersion || '2.26.0') + '.js';
+prebid.src = '//static.amp.services/prebid2.28.0.js';
 var node = document.getElementsByTagName('script')[0];
 node.parentNode.insertBefore(prebid, node);
 
@@ -58,7 +58,7 @@ function initialize() {
         scpt.parentNode.insertBefore(elem, scpt);
         (function () {
             var gdprAppliesGlobally = false;
-            
+
             function addFrame() {
                 if (!window.frames['__cmpLocator']) {
                     if (document.body) {
@@ -75,9 +75,9 @@ function initialize() {
                     }
                 }
             }
-            
+
             addFrame();
-            
+
             function cmpMsgHandler(event) {
                 var msgIsString = typeof event.data === "string";
                 var json;
@@ -101,7 +101,7 @@ function initialize() {
                     });
                 }
             }
-            
+
             window.__cmp = function (c) {
                 var b = arguments;
                 if (!b.length) {
@@ -128,17 +128,17 @@ function initialize() {
                 window.attachEvent('onmessage', cmpMsgHandler);
             }
         })();
-        
+
         // Initialize CMP with custom configuration
         window.__cmp('init', streamampConfig.cmp.config);
-        
+
         // Apply custom CMP styles if true
         if (streamampConfig.cmp.hasCustomStyles && isNotEmptyCmp(streamampConfig.cmp.style)) {
             var style = document.createElement('style');
             var ref = document.querySelector('script');
-            
+
             var quantcastTheme = streamampConfig.cmp.styles;
-            
+
             style.innerHTML =
                 // Background
                 (isNotEmptyCmp(quantcastTheme.ui) && quantcastTheme.ui.backgroundColor
@@ -265,11 +265,10 @@ function initialize() {
                      '}'
                  : '') +
                 '}';
-            
+
             ref.parentNode.insertBefore(style, ref);
         }
     }
-
 
 
 // Initialize apstag
@@ -283,18 +282,18 @@ function initialize() {
 // Set empty arrays for GPT units and codes
     var gptSlots = [];
     var gptSlotsCodes = [];
-    
+
     googletag.cmd.push(function () {
         function singleBreakpointSizeMapping(minWidth, sizesSuppport) {
             return googletag.sizeMapping().addSize([minWidth, 0], sizesSuppport).build()
         }
-        
+
         function allBreakpointsSizeMapping(adunit) {
             return streamampConfig.breakpoints.map(function (breakpoint, index) {
                 return singleBreakpointSizeMapping(breakpoint.minWidth, compareAdUnitBreakpointSizes(adunit, breakpoint.sizesSupported))[0]
             })
         }
-        
+
         function compareAdUnitBreakpointSizes(adUnitSizes, breakpoints) {
             var matchingSizes = []
             breakpoints.forEach(function (breakpoint) {
@@ -306,7 +305,7 @@ function initialize() {
             });
             return matchingSizes
         }
-        
+
         function gptSizeMappingDefineSlots() {
             return streamampConfig.adUnits.map(function (adUnit) {
                 var gptSlot = googletag.defineSlot(adUnit.path, adUnit.mediaTypes.banner.sizes, adUnit.code)
@@ -317,9 +316,9 @@ function initialize() {
                 return gptSlot
             })
         }
-        
+
         gptSizeMappingDefineSlots();
-        
+
         googletag.pubads().disableInitialLoad();
         googletag.pubads().collapseEmptyDivs(streamampConfig.hasCollapsedEmptyDivs);
         googletag.pubads().enableSingleRequest();
@@ -346,21 +345,21 @@ function initialize() {
     function fetchHeaderBids() {
         // Declare header bidders
         var bidders = ['prebid'];
-        
+
         if (streamampConfig.a9Enabled) {
             bidders = ['a9', 'prebid'];
         }
-        
+
         // Keep track of bidders state to determine when to send ad server request
         var requestManager = {
             adserverRequestSent: false,
         };
-        
+
         // Loop through bidder array and add the bidders to the request manager
         bidders.forEach(function (bidder) {
             requestManager[bidder] = false;
         });
-        
+
         // Return true if all bidders have returned
         function allBiddersBack() {
             var allBiddersBack = bidders
@@ -374,7 +373,7 @@ function initialize() {
                 .length === bidders.length;
             return allBiddersBack;
         }
-        
+
         // Handler for header bidder responses
         function headerBidderBack(bidder) {
             // Return early if request to adserver is already sent
@@ -392,7 +391,7 @@ function initialize() {
                 sendAdServerRequest();
             }
         }
-        
+
         // Get ads from GAM
         function sendAdServerRequest() {
             // Return early if request already sent
@@ -407,16 +406,16 @@ function initialize() {
             requestManager.sendAdServerRequest = true;
             // Set bid targeting and make ad request to GAM
             googletag.cmd.push(function () {
-                
+
                 if (streamampConfig.a9Enabled) {
                     apstag.setDisplayBids();
                 }
-                
+
                 pbjs.setTargetingForGPTAsync();
                 googletag.pubads().refresh();
             });
         }
-        
+
         // Request all bids
         function requestBids(apstagSlots, adUnits, bidTimeout) {
             // Request bids from apstag
@@ -428,6 +427,16 @@ function initialize() {
                     headerBidderBack('a9');
                 });
             }
+
+            // Define aliased adapters
+            adUnits.forEach(function (adUnit) {
+                adUnit.bids.forEach(function (bid) {
+                    if (bid.bidder === 'streamamp' || bid.bidder === 'totaljobs') {
+                        pbjs.aliasBidder('appnexus', bid.bidder)
+                    }
+                })
+            })
+
             // Request bids from prebid
             pbjs.que.push(function () {
                 pbjs.addAdUnits(adUnits);
@@ -490,9 +499,9 @@ function initialize() {
                 });
             });
         }
-        
+
         requestBids(apstagSlots, adUnits, bidTimeout);
-        
+
         // Set timeout to send request to call sendAdServerRequest() after timeout if all bidders haven't returned before then
         window.setTimeout(function () {
             sendAdServerRequest();
@@ -564,7 +573,7 @@ function initialize() {
 //       }
 //     }
 //   }
-    
+
     function refreshBids() {
         if (streamampConfig.a9Enabled) {
             apstag.fetchBids({
@@ -605,7 +614,7 @@ function initialize() {
 
 // Refresh bids handler
     window.adRefreshTimer = null;
-    
+
     var refreshAds = function () {
         if (window.adRefreshTimer) {
             window.clearInterval(window.adRefreshTimer);
@@ -617,7 +626,7 @@ function initialize() {
         }, streamampConfig.refreshBidsTimeout * 1e3);
     };
     refreshAds();
-    
+
     window.onfocus = function () {
         refreshAds();
     };
@@ -625,5 +634,5 @@ function initialize() {
         window.clearInterval(window.adRefreshTimer);
         window.adRefreshTimer = null;
     };
-    
+
 };
